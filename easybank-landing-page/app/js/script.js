0a3580bcc56b5c80ebf85d3c90b6fce951c5a58e
ayortanli <ahmetyortanli@gmail.com>
2021-02-10 17:26:40 +0300

    Mobile hamburger menu is created.

--- easybank-landing-page/app/js/script.js
+++ easybank-landing-page/app/js/script.js
@@ -1,16 +1,24 @@
+const body = document.querySelector("body");
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function(){
     if(header.classList.contains("open")) {
         header.classList.remove("open");
-        overlay.classList.remove("fade-in");        
-        overlay.classList.add("fade-out");
+        body.classList.remove("noscroll");
+        fadeElems.forEach( item => {            
+            item.classList.remove("fade-in");        
+            item.classList.add("fade-out");
+        });
     }
     else {
-        header.classList.add("open");
-        overlay.classList.remove("fade-out");        
-        overlay.classList.add("fade-in");
+        header.classList.add("open");        
+        body.classList.add("noscroll");
+        fadeElems.forEach( item => {            
+            item.classList.remove("fade-out");       
+            item.classList.add("fade-in");
+        });
     }
 });
